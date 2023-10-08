42e8bb0fd90eceaa701a5c09c0af52bfe9fa156f
Ayoub RABIYA <rabiyaeayoub@gmail.com>
2023-10-08 16:07:48 +0100

    add main files

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# tip_calculator
-This is a simple tip calculator
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX).
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+### Screenshot
+
+![Screenshot 1](./screenshot1.jpg)
+![Screenshot 2](./screenshot2.jpg)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- JavaScript
+- DOM
+
+## Author
+
+- Frontend Mentor - [@Ayoubrabiae](https://www.frontendmentor.io/profile/Ayoubrabiae)
+- LinkedIn - [Ayoub RABIYA](https://www.linkedin.com/in/ayoub-rabiya/)
---
+++ scripts/main.js
@@ -0,0 +1,73 @@
+const billInp = document.querySelector('#bill');
+const percentSelectors = document.querySelectorAll('.tip-percent');
+const customPercentSelector = document.querySelector('.custom-tip-percent');
+const peopleNumberInp = document.querySelector('#people-number-input');
+const tipAmountArea = document.querySelector('.tip-amount .number');
+const totalArea = document.querySelector('.total .number');
+const resetBtn = document.querySelector('.reset-btn');
+
+const isInputsValid = () => {
+  const condition = billInp.value !== '' && peopleNumberInp.value !== '' && customPercentSelector.parentElement.querySelector('.active');
+  if (condition) {
+    resetBtn.classList.add('active');
+  } else {
+    resetBtn.classList.remove('active');
+  }
+};
+
+const fixInvalidInput = (e) => {
+  if (e.currentTarget.value <= 0) e.currentTarget.value = '';
+  if (e.currentTarget.classList.contains('custom-tip-percent')) {
+    if (e.currentTarget.value >= 100) e.currentTarget.value = '';
+  }
+};
+
+const isError = (e) => {
+  if (!(e.currentTarget.classList.contains('custom-tip-percent'))) {
+    if (e.currentTarget.value === '') {
+      e.currentTarget.parentElement.parentElement.classList.add('error');
+    } else {
+      e.currentTarget.parentElement.parentElement.classList.remove('error');
+    }
+  }
+};
+
+[billInp, customPercentSelector, peopleNumberInp].forEach((e) => e.addEventListener('input', (el) => {
+  fixInvalidInput(el);
+  isInputsValid();
+  isError(el);
+}));
+
+const activateSelector = (e) => {
+  if (e.currentTarget.parentElement.querySelector('.active')) {
+    customPercentSelector.parentElement.querySelector('.active').classList.remove('active');
+  }
+  e.currentTarget.classList.add('active');
+};
+
+percentSelectors.forEach((e) => e.addEventListener('click', (el) => {
+  activateSelector(el);
+  isInputsValid();
+}));
+
+customPercentSelector.addEventListener('blur', (e) => {
+  if (e.currentTarget.value < 100 && e.currentTarget.value > 0) {
+    activateSelector(e);
+    e.currentTarget.dataset.percent = e.currentTarget.value;
+  } else {
+    e.currentTarget.classList.remove('active');
+  }
+});
+
+const calculateTip = () => {
+  const bill = parseFloat(billInp.value);
+  const percent = parseFloat(document.querySelector('.percents .active').dataset.percent);
+  const peopleNumber = parseFloat(peopleNumberInp.value);
+  const tipAmount = ((bill / 100) * percent) / peopleNumber;
+  const total = (bill + (bill / 100) * percent) / peopleNumber;
+
+  totalArea.textContent = `$${Number(total).toFixed(2)}`;
+  tipAmountArea.textContent = `$${Number(tipAmount).toFixed(2)}`;
+};
+
+resetBtn.addEventListener('click', calculateTip);
