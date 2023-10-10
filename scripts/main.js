723862561aba31ba1b103d407d072cb810e36dd2
Ayoub RABIYA <rabiyaeayoub@gmail.com>
2023-10-10 17:28:57 +0100

    fix reset button

--- scripts/main.js
+++ scripts/main.js
@@ -6,9 +6,21 @@ const tipAmountArea = document.querySelector('.tip-amount .number');
 const totalArea = document.querySelector('.total .number');
 const resetBtn = document.querySelector('.reset-btn');
 
+const calculateTip = () => {
+  const bill = parseFloat(billInp.value);
+  const percent = parseFloat(document.querySelector('.percents .active').dataset.percent);
+  const peopleNumber = parseFloat(peopleNumberInp.value);
+  const tipAmount = ((bill / 100) * percent) / peopleNumber;
+  const total = (bill + (bill / 100) * percent) / peopleNumber;
+
+  totalArea.textContent = `$${Number(total).toFixed(2)}`;
+  tipAmountArea.textContent = `$${Number(tipAmount).toFixed(2)}`;
+};
+
 const isInputsValid = () => {
   const condition = billInp.value !== '' && peopleNumberInp.value !== '' && customPercentSelector.parentElement.querySelector('.active');
   if (condition) {
+    calculateTip();
     resetBtn.classList.add('active');
   } else {
     resetBtn.classList.remove('active');
@@ -54,20 +66,21 @@ customPercentSelector.addEventListener('blur', (e) => {
   if (e.currentTarget.value < 100 && e.currentTarget.value > 0) {
     activateSelector(e);
     e.currentTarget.dataset.percent = e.currentTarget.value;
+    isInputsValid();
   } else {
     e.currentTarget.classList.remove('active');
   }
 });
 
-const calculateTip = () => {
-  const bill = parseFloat(billInp.value);
-  const percent = parseFloat(document.querySelector('.percents .active').dataset.percent);
-  const peopleNumber = parseFloat(peopleNumberInp.value);
-  const tipAmount = ((bill / 100) * percent) / peopleNumber;
-  const total = (bill + (bill / 100) * percent) / peopleNumber;
-
-  totalArea.textContent = `$${Number(total).toFixed(2)}`;
-  tipAmountArea.textContent = `$${Number(tipAmount).toFixed(2)}`;
+const reset = () => {
+  billInp.value = '';
+  customPercentSelector.value = '';
+  peopleNumberInp.value = '';
+  if (document.querySelector('.percents .active')) {
+    document.querySelector('.percents .active').classList.remove('active');
+  }
+  tipAmountArea.textContent = '$0.00';
+  totalArea.textContent = '$0.00';
 };
 
-resetBtn.addEventListener('click', calculateTip);
+resetBtn.addEventListener('click', reset);
